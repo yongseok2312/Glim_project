b3c841647c78207858398a1b50f0a2653acfbffe
yongseok2312 <135982451+yongseok2312@users.noreply.github.com>
2024-08-24 16:24:31 +0900

    start

---
+++ 과제 1/CreateCircle/CreateCircle/CCreate.cpp
@@ -0,0 +1,34 @@
+﻿// CCreate.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "CreateCircle.h"
+#include "afxdialogex.h"
+#include "CCreate.h"
+
+
+// CCreate 대화 상자
+
+IMPLEMENT_DYNAMIC(CCreate, CDialogEx)
+
+CCreate::CCreate(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CCreate::~CCreate()
+{
+}
+
+void CCreate::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CCreate, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CCreate 메시지 처리기
---
+++ 과제 1/CreateCircle/CreateCircle/CDraw.cpp
@@ -0,0 +1,34 @@
+﻿// CDraw.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "CreateCircle.h"
+#include "afxdialogex.h"
+#include "CDraw.h"
+
+
+// CDraw 대화 상자
+
+IMPLEMENT_DYNAMIC(CDraw, CDialogEx)
+
+CDraw::CDraw(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CDraw::~CDraw()
+{
+}
+
+void CDraw::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDraw, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDraw 메시지 처리기
